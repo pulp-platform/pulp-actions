f90481dd58e21b1253a45301b83abd7f7cbd1fbd
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2023-04-13 21:28:29 +0200

    lint-license: Add action

---
+++ lint-license/gen_config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Paul Scheffler <paulsc@iis.ee.ethz.ch>
+# Nils Wistoff <nwistoff@iis.ee.ethz.ch>
+
+import sys
+from mako.template import Template
+
+def main(match_regex, license: str, exclude_paths: str = ''):
+    print(f'''
+{{
+    licence:
+        \'\'\'
+{license}
+        \'\'\',
+    match_regex: "{str(bool(match_regex)).lower()}"
+    exclude_paths: [
+        {exclude_paths}
+    ],
+}}
+    ''')
+
+if __name__ == '__main__':
+    sys.exit(main(*sys.argv[1:]))
