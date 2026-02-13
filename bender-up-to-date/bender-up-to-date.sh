7ee6b597bdfa1e480d0f005c12ece43957cb3431
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2026-02-13 18:23:12 +0100

    bender-up-to-date: remove redundant version checks

--- bender-up-to-date/bender-up-to-date.sh
+++ bender-up-to-date/bender-up-to-date.sh
@@ -15,22 +15,10 @@ if ! [[ $(bender --version 2>/dev/null) =~ ([0-9]+\.[0-9]+\.[0-9]+) ]]; then
 fi
 current_version="${BASH_REMATCH[1]}"
 
-bender_args=("script" "--no-deps")
-grep_args=("-v" "-e" "^+")
-
-# Version 0.30.0 introduces metadata to `script` command output to assist with traceability.
-# A dedicated flag `--no-source-annotations` is introduced to suppress such outputs.
-if printf '%s\n' "0.30.0" "$current_version" | sort -C -V; then
-    # Version >= 0.30.0
-    bender_args+=("--no-source-annotations")
-fi
-
-bender_args+=("flist")
-
 # Capture the output AND check the exit code simultaneously.
 # If Bender >= 0.29.0 detects missing files, it will exit with an error code > 0,
 # and this block will catch it and exit immediately.
-if ! BENDER_OUTPUT=$(bender "${bender_args[@]}"); then
+if ! BENDER_OUTPUT=$(bender script --no-deps flist); then
     exit 1
 elif  printf '%s\n' "0.29.0" "$current_version" | sort -C -V; then
     # If we reach here, Bender >= 0.29.0 succeeded (all files exist).
@@ -49,6 +37,6 @@ while IFS= read -r FILE; do
         printf "bender-up-to-date: %s not found.\n" "$FILE"
         RESULT=1
     fi
-done < <(printf "%s\n" "$BENDER_OUTPUT" | grep "${grep_args[@]}" || true)
+done < <(printf "%s\n" "$BENDER_OUTPUT" | grep -v -e "^+" || true)
 
 exit "$RESULT"
